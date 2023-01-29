7d95dc459d8bb12bfe0f8b48f7aa778b6e3602dc
AbekeTurist <usauk2030@gmail.com>
2023-01-29 21:35:13 +0000

    task 4 yapildi

--- src/Gun01/Student.java
+++ src/Gun01/Student.java
@@ -4,7 +4,7 @@ public class Student {
     public static void main(String[] args) {
 
         System.out.println("Merhaba 1");
-        System.out.println("Task 2 yapildi ");
+        System.out.println("Task 2 yapildi");
         System.out.println("tekrar Merhaba");
     }
 }
